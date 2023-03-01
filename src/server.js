34b4e1e315c6cd8ccb7bfc215ec049cee6ccc965
Gustavo Vieira <gustavo.vieira0909@gmail.com>
2023-03-01 12:05:06 -0300

    Express server v2.0

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
-const express = require("express")
-const app = express()
-const PORT = 3000
+const express = require("express")//Import express
+const app = express()//Starts express
+const PORT = 3000//Define the web PORT
 
-app.get("/ender/:id/:user" , (request, response)=>{
-  const {id, user} = request.params
-  response.send(`Hello, World! - Message's ID is ${id} - User: ${user}`)
+ 
+
+app.listen(PORT, ()=>{//Express listen the PORT)
+  console.log(`Server is running on port ${PORT}`)
 })
 
-app.listen(PORT, ()=>{
-  console.log(`Server is running on port ${PORT}`)
+app.get("/ender/:id/:user" , (req, res)=>{ //Express Source's response 
+  const {id, user} = req.params//Destructure the req.params
+  res.send(`Hello, World! - Message's ID is ${id} - User: ${user}`)//This is the res
+})
+
+app.get("/users", (req, res)=>{
+  const { page, limit } = req.query
+  res.send(`Page ${page}. Shows: ${limit}`)
 })
