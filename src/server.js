c4511a1adb427be9edd37d72e1032c96ceffae15
Gustavo Vieira <gustavo.vieira0909@gmail.com>
2023-03-02 20:05:28 -0300

    v4

--- src/server.js
+++ src/server.js
@@ -2,18 +2,20 @@ const express = require("express")//Import express
 const app = express()//Starts express
 const PORT = 3000//Define the web PORT
 
- 
+app.use(express.json())
 
 app.listen(PORT, ()=>{//Express listen the PORT)
   console.log(`Server is running on port ${PORT}`)
 })
 
-app.get("/ender/:id/:user" , (req, res)=>{ //Express Source's response 
-  const {id, user} = req.params//Destructure the req.params
-  res.send(`Hello, World! - Message's ID is ${id} - User: ${user}`)//This is the res
+app.post("/users", (req, res)=>{
+  const { name, yo} = req.body
+  // res.send(`User: ${name}, YO: ${yo}`) 
+  res.json({ name, yo})
 })
 
-app.get("/users", (req, res)=>{
-  const { page, limit } = req.query
-  res.send(`Page: ${page}, look:${limit}`)
-})
+
+// app.get("/ender/:id/:user" , (req, res)=>{ //Express Source's response 
+//   const {id, user} = req.params//Destructure the req.params
+//   res.send(`Hello, World! - Message's ID is ${id} - User: ${user}`)//This is the res
+// })
