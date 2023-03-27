b234d0e68fece096ebdf36c5bace736a5077c05d
Gustavo Vieira <gustavo.vieira0909@gmail.com>
2023-03-26 23:45:52 -0300

    A

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,13 +1,20 @@
+//Imported AppError from utils
 const AppError = require("../utils/AppError")
 
+//Class responsible for create user
 class UsersController{
   create(request, response){
     const { name, yo, email} = request.body
   
+    //Check if the name exists, than throw the AppError that had imported previously
     if(!name){
       throw new AppError("Nome Obrigatório!")
     }
+
+    // If there is no error, give the answer with 200 status code and a .json
     response.status(200).json({name, yo, email })
   }
 }
+
+//Export the UserController class
 module.exports = UsersController
